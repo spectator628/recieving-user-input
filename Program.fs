b34a73e4ff1efe7bce4d60f0674f683927f15a1a
spectator628 <46543210+spectator628@users.noreply.github.com>
2019-01-27 23:55:36 -0500

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hey whats your name?"
+
+    let name = Console.ReadLine()
+
+    printfn "Where are you from?"
+
+    let address = Console.ReadLine();
+
+    printfn "Where are you visiting?"
+
+    let visiting = Console.ReadLine();
+
+    printfn "So %s, you're here visiting %s from %s, how nice, have a greagt trip!" name address visiting
+
+    0 // return an integer exit code
